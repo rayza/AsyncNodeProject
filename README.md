67e28cbb9bda3c8b4c58a6a23e988938d84dd1a9
Raymund Zacharias <rayza8891@yahoo.de>
2023-06-16 12:53:36 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# AsyncNodeProject
+
+Developed with Unreal Engine 5
---
+++ Source/AsyncNodeProject/AsyncNodeProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AsyncNodeProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ASYNCNODEPROJECT_API AAsyncNodeProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
